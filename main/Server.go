276622ada46d18657c64c0f8433e7630c510b93c
Sepehr Sabour <sepehr.sabour@gmail.com>
2020-03-05 21:24:43 -0700

    Add config file :art:

--- main/Server.go
+++ main/Server.go
@@ -2,9 +2,24 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/tkanos/gonfig"
 )
 
+type Configuration struct {
+	Port              string
+}
+
+
 func main() {
+
+	//Read Config File
+	configuration := Configuration{}
+	err := gonfig.GetConf("config/config.development.json", &configuration)
+	if err != nil {
+		println(err.Error())
+		return
+	}
+
 	gin.SetMode(gin.DebugMode)
 	router := gin.Default()
 	router.GET("/echo/:name", func(c *gin.Context) {
@@ -14,5 +29,6 @@ func main() {
 		})
 	})
 
-	router.Run(":1234")
+	println("Starting Server on Port: " + configuration.Port)
+	router.Run(configuration.Port)
 }
---
+++ main/config.go
@@ -0,0 +1,6 @@
+
+package main
+
+//type Configuration struct {
+//	Port              string
+//}
