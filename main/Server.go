062f0281e9f7892a1a220f0280212edcba13af62
Sepehr Sabour <sepehr.sabour@gmail.com>
2020-03-05 21:07:50 -0700

    Add gin library :sparkles:

--- main/Server.go
+++ main/Server.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+)
 
+func main() {
+	gin.SetMode(gin.DebugMode)
+	router := gin.Default()
+	router.GET("/echo/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.JSON(200, gin.H{
+			"message": "Hello " + name,
+		})
+	})
+
+	router.Run(":1234")
 }
